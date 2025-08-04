aecbb612f400f105e2aa7dff31e4f88ae27ce1c4
Dishank Patil <dishankjp05@gmail.com>
2025-08-04 20:47:58 +0530

    changing index variable

--- market_predictor.py
+++ market_predictor.py
@@ -4,26 +4,26 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score 
 
-sp500 = yf.Ticker("^BSESN")
-sp500 = sp500.history(period="max")
+spBSE = yf.Ticker("^BSESN")
+spBSE = spBSE.history(period="max")
 
-# sp500.plot(y="Close", use_index=True)
+# spBSE.plot(y="Close", use_index=True)
 # plt.show()
 
-del sp500["Dividends"]
-del sp500["Stock Splits"]
+del spBSE["Dividends"]
+del spBSE["Stock Splits"]
 
-sp500["NextDayClose"] = sp500["Close"].shift(-1)
-sp500["Target"] = (sp500["NextDayClose"] > sp500["Close"]).astype(int)
+spBSE["NextDayClose"] = spBSE["Close"].shift(-1)
+spBSE["Target"] = (spBSE["NextDayClose"] > spBSE["Close"]).astype(int)
 
-sp500 = sp500.loc["2000-01-01":].copy()
+spBSE = spBSE.loc["2000-01-01":].copy()
 
-# print(sp500)
+# print(spBSE)
         
 model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
 
-train = sp500.iloc[:-100]    
-test = sp500.iloc[-100:]    
+train = spBSE.iloc[:-100]    
+test = spBSE.iloc[-100:]    
 
 predictors = ["Open", "High", "Low", "Close", "Volume"]
 model.fit(train[predictors],train["Target"])
@@ -57,7 +57,7 @@ def backtest(data,model,predictors,start=2500,step=250):
         all_predictions.append(predictions)
     return pd.concat(all_predictions)
 
-predictions = backtest(sp500,model,predictors)
+predictions = backtest(spBSE,model,predictors)
 
 # noOfPredictions = predictions["Predictions"].value_counts() 
 # print(noOfPredictions) 
@@ -72,19 +72,19 @@ horizons = [2,5,250,1000]
 new_predictors = []
 
 for hor in horizons:
-    rolling_avg = sp500.rolling(hor).mean()
+    rolling_avg = spBSE.rolling(hor).mean()
 
     ratio_column = f"Close_Ratio_{hor}"
-    sp500[ratio_column] = sp500["Close"]/rolling_avg["Close"]
+    spBSE[ratio_column] = spBSE["Close"]/rolling_avg["Close"]
 
     trend_column = f"Trend_{hor}"
-    sp500[trend_column] = sp500.shift(1).rolling(hor).sum()["Target"]
+    spBSE[trend_column] = spBSE.shift(1).rolling(hor).sum()["Target"]
     
     new_predictors+=[ratio_column,trend_column]
 
-# sp500 = sp500.dropna()
+# spBSE = spBSE.dropna()
 
-new_predictions = backtest(sp500,model,new_predictors)
+new_predictions = backtest(spBSE,model,new_predictors)
 
 precisionScore = precision_score(new_predictions["Target"],new_predictions["Predictions"]) 
 print("Precison Score: ",precisionScore)
@@ -92,4 +92,4 @@ print("Precison Score: ",precisionScore)
 noOfNewPredictions = new_predictions["Predictions"].value_counts()
 print(noOfNewPredictions)
 
-print(sp500)
+print(spBSE)
